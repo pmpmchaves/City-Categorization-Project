d64439020b3eb92351d536f9593ee4502938e331
Rodrigo Pinto <rodimendes@gmail.com>
2022-09-06 09:22:48 +0100

    Last 5-09

--- frontend/app_sentinel.py
+++ frontend/app_sentinel.py
@@ -7,6 +7,47 @@ import os
 import streamlit as st
 import pandas as pd
 
+##############################################
+## Setting page configurations on Streamlit ##
+##############################################
+st.set_page_config(
+            page_title="City Categorization",
+            page_icon="🌎",
+            layout="centered", # wide
+            initial_sidebar_state="auto") # collapsed
+
+
+CSS = """
+h1, h2 {
+    text-align: center;
+}
+h4 {
+    text-align: center;
+}
+h6 {
+    text-align: center;
+    font-size: 16px;
+    font-weight: 100;
+}
+p {
+    text-align: center;
+}
+"""
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+CLIENT_ID = 'b3d396e1-a257-4489-9c1d-7d24177c05e7'
+CLIENT_SECRET = "pF;ajNy(p)vt;mi5;PQE3i}+1y+8wH|lw,lND0_8"
+X_API_KEY = 'cf4NxGcWpzoxoxQdiHZjfg==m0jURpBpDRHpKBBN'
+
+# set up credentials
+client = oauthlib.oauth2.BackendApplicationClient(client_id=CLIENT_ID)
+oauth = requests_oauthlib.OAuth2Session(client=client)
+
+# get an authentication token
+token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/oauth/token',
+                        client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+
 def get_rect(lat, lon, pop):
     if pop > 10000000:
         increase = 0.06
@@ -107,48 +148,6 @@ def get_satellite_image(city):
 
     columns[0].success('The search is over! 🎯 🏆')
 
-
-CSS = """
-h1, h2 {
-    text-align: center;
-}
-h4 {
-    text-align: center;
-}
-h6 {
-    text-align: center;
-    font-size: 16px;
-    font-weight: 100;
-}
-p {
-    text-align: center;
-}
-"""
-
-CLIENT_ID = 'b3d396e1-a257-4489-9c1d-7d24177c05e7'
-CLIENT_SECRET = "pF;ajNy(p)vt;mi5;PQE3i}+1y+8wH|lw,lND0_8"
-X_API_KEY = 'cf4NxGcWpzoxoxQdiHZjfg==m0jURpBpDRHpKBBN'
-
-# set up credentials
-client = oauthlib.oauth2.BackendApplicationClient(client_id=CLIENT_ID)
-oauth = requests_oauthlib.OAuth2Session(client=client)
-
-# get an authentication token
-token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/oauth/token',
-                        client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-
-##############################################
-## Setting page configurations on Streamlit ##
-##############################################
-st.set_page_config(
-            page_title="City Categorization",
-            page_icon="🌎",
-            layout="centered", # wide
-            initial_sidebar_state="auto") # collapsed
-
-
-#Containers
-st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 ###############
 ## Main code ##
 ###############
@@ -169,27 +168,10 @@ columns[0].write('#### OR')
 
 city = columns[0].text_input('City name 🌎')
 if columns[0].button('Click me to find the city satellite image'):
-    uploaded_file.close()
     get_satellite_image(city=city)
     image = Image.open(f'{city}.tiff')
     columns[1].image(image, caption=city.capitalize())
 
-# if uploaded_file is not None:
-#      # To read file as bytes:
-#      bytes_data = uploaded_file.getvalue()
-#      st.write(bytes_data)
-
-#      # To convert to a string based IO:
-#      stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-#      st.write(stringio)
-
-#      # To read file as string:
-#      string_data = stringio.read()
-#      st.write(string_data)
-
-#      # Can be used wherever a "file-like" object is accepted:
-#      dataframe = pd.read_csv(uploaded_file)
-#      st.write(dataframe)
 ############################################
 ## Access the model e get processed image ##
 ############################################
